4d94365979e7dc23e4a474372193efad5981b8f6
Hrvoje Vincek <vincekhrvoje@hervo.local>
2023-09-12 10:11:34 +0200

    refreactoring

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import GoogleMap from "./GoogleMap/page";
+import GoogleMapRenders from "./components/GoogleMapRenders";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="mb-5">HOLA PAPI</h1>
-      <GoogleMap />
+      <GoogleMapRenders />
     </main>
   );
 }
